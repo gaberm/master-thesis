03c95ba963e62e053e59fd371a5ed990597c8783
Malte Gaber <malte.gaber@proton.me>
2024-02-20 17:37:46 +0100

    Fixed small error caused by renaming the uncertainty metric parameter

--- src/metric.py
+++ src/metric.py
@@ -18,21 +18,21 @@ def load_metric(config, metric_type):
     device = config.trainer.gpu_name[platform.system()]
 
     match (metric_type, metric_name, num_labels):
-        case ("val", "f1", 2):
+        case ("pred", "f1", 2):
             metric = F1Score(task="binary").to(device)
-        case ("val", "f1", _):
+        case ("pred", "f1", _):
             metric = F1Score(task="multiclass", num_classes=num_labels, average="micro").to(device)
-        case ("val", "accuracy", 2):
+        case ("pred", "accuracy", 2):
             metric = Accuracy(task="binary").to(device)
-        case ("val", "accuracy", _):
+        case ("pred", "accuracy", _):
             metric = Accuracy(task="multiclass", num_classes=num_labels, average="micro").to(device)
-        case ("uncertainty", "expected_calibration_error", 2):
+        case ("uncert", "ece", 2):
             metric = BinaryCalibrationError(n_bins=10, norm="l1").to(device)
-        case ("uncertainty", "expected_calibration_error", _):
+        case ("uncert", "ece", _):
             metric = MulticlassCalibrationError(num_classes=num_labels, n_bins=10, norm="l1").to(device)
-        case ("uncertainty" "maximum_calibration_error", 2):
+        case ("uncert" "mce", 2):
             metric = BinaryCalibrationError(n_bins=15, norm="max").to(device)
-        case ("uncertainty", "maximum_calibration_error", _):
+        case ("uncert", "mce", _):
             metric = MulticlassCalibrationError(num_classes=num_labels, n_bins=10, norm="max").to(device)
         case (_, _, _):
             raise ValueError(f"Metric {metric_name} not supported.")
