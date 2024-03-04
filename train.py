fb566cbe8ca635c4c0885f36efc3e163a175b3cf
Malte Gaber <malte.gaber@proton.me>
2024-03-04 10:29:14 +0100

    Training iterates over 5 seeds

--- train.py
+++ train.py
@@ -13,47 +13,49 @@ dotenv.load_dotenv(".env")
 
 @hydra.main(config_path="conf", config_name="config", version_base="1.3")
 def main(config):
-    # seed everything for reproducibility
-    pl.seed_everything(config.params.seed, workers=True) 
+    # run the experiment for 5 different seeds
+    for seed in config.params.seed:
+        pl.seed_everything(seed, workers=True) 
 
-    print(config)
+        # print the config for the current run
+        print(config)
 
-    # gpu, no of devices and training strategy differ between the macos and linux setup
-    # so we need to know which system we are on
-    system = platform.system().lower()
+        # gpu, no of devices and training strategy differ between the macos and linux setup
+        # so we need to know which system we are on
+        system = platform.system().lower()
 
-    model = load_model(config)
-    tokenizer = load_tokenizer(config)
-    train_loader, val_loader = create_train_loader(config, tokenizer)
-    
-    # create lightning model and initialize wandb logger
-    l_model = LModel(model, config)
-    wandb_logger = WandbLogger(project=config.wandb.project)
+        model = load_model(config)
+        tokenizer = load_tokenizer(config)
+        train_loader, val_loader = create_train_loader(config, tokenizer)
+        
+        # create lightning model and initialize wandb logger
+        l_model = LModel(model, config)
+        wandb_logger = WandbLogger(project=config.wandb.project)
 
-    ckpt_dir = f"{config.data_dir[system]}/checkpoints/{config.trainer.exp_name}/seed_{config.params.seed}"
-    # check to avoid overwriting of existing checkpoints
-    if os.path.exists(ckpt_dir):
-        raise ValueError(f"Checkpoint directory {ckpt_dir} already exists. Please delete or change it.")
-    
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=ckpt_dir,
-        monitor=config.params.pred_metric,
-        mode="max",
-        filename=f"{{epoch}}-{{step}}-{{{config.params.pred_metric}:.3f}}",
-        save_top_k=config.trainer.save_top_k)
-    lr_callback = LearningRateMonitor(logging_interval="step")
+        ckpt_dir = f"{config.data_dir[system]}/checkpoints/{config.trainer.exp_name}/seed_{config.params.seed}"
+        # check to avoid overwriting of existing checkpoints
+        if os.path.exists(ckpt_dir):
+            raise ValueError(f"Checkpoint directory {ckpt_dir} already exists. Please delete or change it.")
+        
+        checkpoint_callback = ModelCheckpoint(
+            dirpath=ckpt_dir,
+            monitor=config.params.pred_metric,
+            mode="max",
+            filename=f"{{epoch}}-{{step}}-{{{config.params.pred_metric}:.3f}}",
+            save_top_k=config.trainer.save_top_k)
+        lr_callback = LearningRateMonitor(logging_interval="step")
 
-    trainer = pl.Trainer(max_epochs=config.trainer.max_epochs,
-                        logger=wandb_logger, 
-                        default_root_dir=config.data_dir[system],
-                        deterministic=True,
-                        strategy=config.trainer.strategy[system],
-                        devices=config.trainer.devices[system],
-                        val_check_interval=0.25,
-                        callbacks=[checkpoint_callback, lr_callback])
-    
-    # train the model
-    trainer.fit(model=l_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+        trainer = pl.Trainer(max_epochs=config.trainer.max_epochs,
+                            logger=wandb_logger, 
+                            default_root_dir=config.data_dir[system],
+                            deterministic=True,
+                            strategy=config.trainer.strategy[system],
+                            devices=config.trainer.devices[system],
+                            val_check_interval=0.25,
+                            callbacks=[checkpoint_callback, lr_callback])
+        
+        # train the model
+        trainer.fit(model=l_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
 if __name__ == "__main__":
     main()
