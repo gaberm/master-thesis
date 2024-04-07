70a861db464729df90a3a5ba5b1bcb012eb29503
Malte Gaber <malte.gaber@proton.me>
2024-04-07 19:15:48 +0200

    Removed optimizer file because it was unnecessary. We only use adamW

--- src/optimizer.py
+++
@@ -1,8 +0,0 @@
-import torch.optim as optim
-
-def load_optimizer(model, optimizer, lr):
-    match optimizer:
-        case "adamW":
-            return optim.AdamW(model.parameters(), lr)
-        case _:
-            raise ValueError("Unsupported optimizer")
