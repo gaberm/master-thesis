a17b91287c14c5b49a9be4fd8eae6ba76e838b28
Malte Gaber <malte.gaber@proton.me>
2024-04-17 11:57:14 +0200

    New function which merges all test result csv

---
+++ merge.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+def main():
+    all_results = []
+    for file in os.listdir("res"):
+        if os.path.isfile(os.path.join("res", file)):
+            all_results.append(file)
+    
+    final_df = pd.DataFrame(columns=["exp_name", "seed", "target_lang", "metric", "score"])
+    for file in all_results:
+        df = pd.read_csv(f"res/{file}", sep=";", decimal=",")
+        final_df = pd.concat([final_df, df], axis=0).reset_index(drop=True)
+
+    final_df.to_csv("all_results.csv", sep=";", decimal=",", index=False)
+
+
+if __name__ == "__main__":
+    main()
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -5,6 +5,7 @@ dir="conf/exp/test"
 for file in "$dir"/*
 do
     filename=$(basename -- "$file")
-    echo "Processing $filename"
+    echo "Testing $filename"
     python test.py +exp=test/$filename
+    python merge.py
 done
